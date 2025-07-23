f54f20a9ab1c0c5c5cd55aea338899f6da19398b
icerdayl <ahiezerdayldalangin@gmail.com>
2025-07-23 01:08:46 -0700

    Scrimba Test

---
+++ JavaScript Files/Build a Chrome Extension/Test 3/index.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("container")
+
+container.innerHTML = "<button>Buy!</button>"
+
+function buy(){
+    container.innerHTML += "<p> Thank you for Buying!</p>"
+}
---
+++ JavaScript Files/Build a Chrome Extension/Test 4.js
@@ -0,0 +1,6 @@
+const recipient = "James"
+const sender = "Ahiezer"
+
+const email = `Hey ${recipient}! How is it going? Cheers ${sender} `
+
+console.log(email)
