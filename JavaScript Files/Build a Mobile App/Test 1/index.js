96e3faaf4d5bfab6b94bca6187b5af22fb8bf01e
icerdayl <ahiezerdayldalangin@gmail.com>
2025-07-24 00:00:19 -0700

    Scrimba Test

---
+++ JavaScript Files/Build a Mobile App/Test 1/index.js
@@ -0,0 +1,52 @@
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js"
+    import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js"
+
+    const firebaseConfig = {
+        databaseURL: "https://leads-tracker-app-961fc-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    }
+
+    const app = initializeApp(firebaseConfig)
+    const database = getDatabase(app)
+    const referenceInDB = ref (database, "leads")
+
+    console.log(database)
+
+    let myLeads = []
+    const inputEl = document.getElementById("input-el")
+    const inputBtn = document.getElementById("input-btn")
+    const ulEl = document.getElementById("ul-el")
+    const deleteBtn = document.getElementById("delete-btn")
+
+
+    function render(leads) {
+        let listItems = ""
+        for (let i = 0; i < leads.length; i++) {
+            listItems += `
+                <li>
+                    <a target='_blank' href='${leads[i]}'>
+                        ${leads[i]}
+                    </a>
+                </li>
+            `
+        }
+        ulEl.innerHTML = listItems
+    }
+
+    onValue(referenceInDB, function(snapshot){
+        const snapshotExists = snapshot.exists()
+        if (snapshotExists){
+            const snapshotValues = snapshot.val()
+            const leads = Object.values(snapshotValues)
+            render(leads)
+        }
+    })
+
+    deleteBtn.addEventListener("click", function() {
+        remove(referenceInDB)
+            ulEl.innerHTML = ""
+    })
+
+    inputBtn.addEventListener("click", function() {
+        push(referenceInDB, inputEl.value)
+        inputEl.value = ""
+    })
